ae026d7367d6c02bd2aa99611e86436802050eeb
Crom <doyoung.yang2005@gmail.com>
2026-06-28 00:01:30 +0900

    Chore : redis, s3, openai 설정추가

--- src/main/java/com/pawmesh/backend/PawMeshBackendApplication.java
+++ src/main/java/com/pawmesh/backend/PawMeshBackendApplication.java
@@ -2,8 +2,10 @@ package com.pawmesh.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class PawMeshBackendApplication {
 
     public static void main(String[] args) {
